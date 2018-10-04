d15d8fe52a76abf87c86387be53c60ca79a937cf
Bruce Huang 黃耀進 <12061117@cecdm.com.tw>
2018-10-04 17:09:17 +0800

    init

---
+++ dotnet-myApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
